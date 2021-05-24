1b589e3e8b5bfb945cc5fa31e9cde03ba84f1798
l.adomat <l.adomat@reply.de>
2021-05-24 14:20:59 +0200

    removed cookies and addes sessions

--- app.py
+++ app.py
@@ -1,56 +1,54 @@
-from flask import Flask, render_template, request, make_response
+from flask import Flask, render_template, request, session
 from controller.GameHandler import GameHandler
 from controller.User import User
 from controller.PageHandler import PageHandler
 
 app = Flask(__name__)
+app.secret_key = "secret_key_for_the_sessions"
 pageHandler = PageHandler()
 gameHandler = GameHandler()
 
 
 @app.route('/')
 def main_page():
-	username = request.cookies.get('userName')
-	if username:
+	if "username" in session:
+		username = session["username"]
 		user = pageHandler.getUser(username)
 		if user:
 			return render_template('index.html', user=user)
 
 	user = User(randomPlayer=True)
 	pageHandler.addUser(user)
-	resp = make_response(render_template('index.html', user=user))
-	resp.set_cookie('userName', user.userName)
-	return resp
+	session["username"] = user.userName
+	return render_template('index.html', user=user)
 
 
 @app.route('/impressum')
 def impressum_page():
-	username = request.cookies.get('userName')
-	if username:
+	if "username" in session:
+		username = session["username"]
 		user = pageHandler.getUser(username)
 		if user:
 			return render_template('impressum.html', user=user)
 
 	user = User(randomPlayer=True)
 	pageHandler.addUser(user)
-	resp = make_response(render_template('impressum.html', user=user))
-	resp.set_cookie('userName', user.userName)
-	return resp
+	session["username"] = user.userName
+	return render_template('impressum.html', user=user)
 
 
 @app.route('/contact')
 def contact_page():
-	username = request.cookies.get('userName')
-	if username:
+	if "username" in session:
+		username = session["username"]
 		user = pageHandler.getUser(username)
 		if user:
 			return render_template('contact.html', user=user)
 
 	user = User(randomPlayer=True)
 	pageHandler.addUser(user)
-	resp = make_response(render_template('contact.html', user=user))
-	resp.set_cookie('userName', user.userName)
-	return resp
+	session["username"] = user.userName
+	return render_template('contact.html', user=user)
 
 
 @app.route('/game/<id>')
@@ -60,17 +58,16 @@ def game_page():
 
 @app.route('/history')
 def history_page():
-	username = request.cookies.get('userName')
-	if username:
+	if "username" in session:
+		username = session["username"]
 		user = pageHandler.getUser(username)
 		if user:
 			return render_template('history.html', user=user)
 
 	user = User(randomPlayer=True)
 	pageHandler.addUser(user)
-	resp = make_response(render_template('history.html', user=user))
-	resp.set_cookie('userName', user.userName)
-	return resp
+	session["username"] = user.userName
+	return render_template('history.html', user=user)
 
 
 if __name__ == '__main__':
