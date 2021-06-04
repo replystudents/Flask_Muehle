ba32dc2ac6b4bb907e023278cdcf9cbc51f3c3cc
l.adomat <l.adomat@reply.de>
2021-06-04 16:49:20 +0200

    Add some game Rules

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import eventlet
 app = Flask(__name__)
 app.secret_key = "secret_key_for_the_sessions"
 app.config[
-    'SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/muehle_db.sqlite'  # 'sqlite:////tmp/muehle_db.sqlite'  # might be different in windows
+    'SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/muehle_db.sqlite'  # mac: 'sqlite:////tmp/muehle_db.sqlite'  # windows: 'sqlite:///C:\\dbtest\\foo.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
